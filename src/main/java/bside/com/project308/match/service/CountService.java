d9678a43339479116f786ac997b0f1d442189877
galmegiz <somethingsjw@gmail.com>
2023-08-05 22:12:23 +0900

    feat : feature#3 - 3 현재 사용한 매치 count 관리하는 기능 구현

---
+++ src/main/java/bside/com/project308/match/service/CountService.java
@@ -0,0 +1,36 @@
+package bside.com.project308.match.service;
+
+import bside.com.project308.common.constant.ResponseCode;
+import bside.com.project308.common.exception.InvalidAccessException;
+import bside.com.project308.common.exception.ResourceNotFoundException;
+import bside.com.project308.match.entity.Count;
+import bside.com.project308.match.repository.CountRepository;
+import bside.com.project308.member.entity.Member;
+import bside.com.project308.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class CountService {
+
+    private final CountRepository countRepository;
+    private final MemberRepository memberRepository;
+    public Integer matchUserAndGetMatchCount(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new ResourceNotFoundException(ResponseCode.MEMBER_NOT_FOUND));
+        Count memberCount = countRepository.findByMember(member).orElseGet(() -> Count.of(member));
+
+        if(memberCount.isExhausted()){
+            throw new InvalidAccessException(HttpStatus.BAD_REQUEST, ResponseCode.MATCH_COUNT_EXHAUSTED);
+        }
+
+        memberCount.useMatch();
+        return memberCount.getCount();
+
+    }
+}
