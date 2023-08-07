047a24822d09f894bba50ca652ccb939e5987d58
galmegiz <somethingsjw@gmail.com>
2023-08-07 12:14:06 +0900

    feat: feaure # 6 - 1 로그인/로그아웃 및 세션 보안설정 추가 - 로그인 시 입력값이 없으면 에러 response 반환 - 로그인 시 로그 남기도록 설정 - logout success handler 추가

--- src/main/java/bside/com/project308/security/security/CustomLogoutSuccessHandler.java
+++ src/main/java/bside/com/project308/security/security/CustomLogoutSuccessHandler.java
@@ -2,35 +2,35 @@ package bside.com.project308.security.security;
 
 import bside.com.project308.common.constant.ResponseCode;
 import bside.com.project308.common.response.Response;
+import bside.com.project308.security.filter.CustomLoginFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import java.io.IOException;
 
 @Slf4j
-public class CustomAccessDeniedHandler implements AccessDeniedHandler {
-
+@RequiredArgsConstructor
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
     private final ObjectMapper objectMapper;
 
-    public CustomAccessDeniedHandler(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-      log.info("access dined");
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("[] {} logout", ((UserPrincipal) authentication.getPrincipal()).id(), ((UserPrincipal) authentication.getPrincipal()).getUsername());
+
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
+        //front 요청으로 모든 응답은 200으로
         response.setStatus(HttpStatus.OK.value());
 
-        Response errorResponse = Response.failResponse(ResponseCode.NOT_AUTHORIZED.getCode(), ResponseCode.NOT_AUTHORIZED.getDesc());
+        Response errorResponse = Response.failResponse(ResponseCode.LOGOUT_SUCCESS.getCode(), ResponseCode.LOGOUT_SUCCESS.getDesc());
         String body = objectMapper.writeValueAsString(errorResponse);
         response.getWriter().println(body);
     }
